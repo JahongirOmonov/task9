c432508a1aaf26e87b3866ff744b3979058e19e2
Jahongir <dodajoy0777@gmail.com>
2023-09-02 04:03:03 +0500

    qaleee

--- polls/views.py
+++ polls/views.py
@@ -2,23 +2,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import notebooks, Bolalar
 from django.http import JsonResponse
+from .serializer import notebooksSerializer, bolalarSerializer
 
 # Create your views here.
 
 def all(request):
-    kalla=[]
-    qoyil=notebooks.objects.all()
-    for i in qoyil:
-        kalla.append({
-            'name':i.name,
-            'count':i.count
-        })
-    return JsonResponse(kalla, safe=False)
+    all=notebooks.objects.all()
+    result=notebooksSerializer(all, many=True)
+    # for i in qoyil:
+    #     kalla.append({
+    #         'name':i.name,
+    #         'count':i.count
+    #     })
+    return JsonResponse(result.data, safe=False)
     
 
 
 def detail(request, myid):
-    each = get_object_or_404(Bolalar, id=myid)
-    data={'name':each.name, 'number':each.number}
-    return JsonResponse(data, safe=False)
+    some = Bolalar.objects.filter(id=myid)
+    forgett = bolalarSerializer(some, many=True)
+    # each = get_object_or_404(Bolalar, id=myid)
+    # data={'name':each.name, 'number':each.number}
+    return JsonResponse(forgett.data, safe=False)
         
